5ad19596db80e816a70539448251f65902d8c65f
EugeniaYang <1227390213@qq.com>
2022-02-06 15:51:13 -0500

    finished problem set 3

--- Problem Set 3/myJS.js
+++ Problem Set 3/myJS.js
@@ -2,9 +2,9 @@
 * Excercise 1
 *
 */
-
-
-
+let newColor = false;
+let colorBlock=document.getElementById('color-block');
+colorBlock.addEventListener("click",changeColor);
 /*
 * Then write a function that changes the text and the color inside the div
 *
@@ -12,17 +12,19 @@
 
 function changeColor(){
     //Write a condition determine what color it should be changed to
-    if(){
+    if(newColor==false){
         //change the background color using JS
-
+        colorBlock.style.backgroundColor="#605a9f";
+        newColor=true;
         //Change the text of the color using the span id color-name
-
+        document.getElementById('color-name').textContent="#605a9f";
     }
     else{
         //change the background color using JS
-
+        colorBlock.style.backgroundColor= "#F08080";
+        newColor=false;
         //Change the text of the color using the span id color-name
-
+        document.getElementById('color-name').textContent="#F08080";
 
     }
 }
@@ -33,7 +35,8 @@ function changeColor(){
 * on mouse click. For best practice use addEventListener.
 *
 */
-
+let convertBtn=document.getElementById('convertbtn');
+convertBtn.addEventListener("click",convertTemp);
 
 /*
 * Then write a function that calculates Fahrenheit to Celsius and display it on the webpage
@@ -42,9 +45,9 @@ function changeColor(){
 
 function convertTemp(){
     //Calculate the temperature here
-
+    let tempCelcius=(document.getElementById('f-input').value-32)*5/9;
     //Send the calculated temperature to HTML
-
+    document.getElementById('c-output').textContent=tempCelcius;
 }
 
 
