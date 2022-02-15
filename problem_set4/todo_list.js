245aa777cbb8b2979a7fc45d79078c2c265ab653
EugeniaYang <1227390213@qq.com>
2022-02-14 19:41:15 -0500

    completed all requiremetns

--- problem_set4/todo_list.js
+++ problem_set4/todo_list.js
@@ -1,43 +1,52 @@
 
 task_list = document.getElementById("task_list");
-// addTask("Learn to wrap gifts", 1639944400000);
-// addTask("Buy milk");
+addTask("Learn to wrap gifts", 1639944400000);
+addTask("Buy milk");
+const add_btn = document.getElementById("add_task");
 
-const btn = document.getElementById("add_task");
+// Execute a function when the user releases a key on the keyboard
+document.addEventListener("keydown", function(event) {
+  // Number 13 is the "Enter" key on the keyboard
+  if (event.keyCode === 13) {
+    // Cancel the default action, if needed
+    event.preventDefault();
+    // Trigger the button element with a click
+    add_btn.click();
+    console.log('keydown')
+  }
+});
 
-btn.addEventListener("click", function(){
+
+add_btn.addEventListener("click", () => {
     console.log("button clicked");
-    if (dueTime = dateAndTimeToTimestamp(
-        document.getElementById("duedate_input"),
-        document.getElementById("duetime_input")
-        )){
-        addTask(
-            document.getElementById("task_description_input").value, 
-            dueTime
-        )
-    }
-    else {
-        addTask(
-            document.getElementById("task_description_input").value
-        )
-    }
+    dueTime = dateAndTimeToTimestamp(document.getElementById("duedate_input"),document.getElementById("duetime_input"));
+    addTask(document.getElementById("task_description_input").value, dueTime)
+    document.getElementById("task_description_input").value=''
 }
 );
 
-
 function addTask(description, dueTime){
-    console.log(description,dueTime)
+    console.log(description, dueTime)
     // add a new item to the ul#task_list element
     const new_task = document.createElement("li");
-    // new_task.id=""
     if (dueTime){
         const d = new Date();
         d.setTime(dueTime)
-        new_task.innerHTML += description + "<span class=\"due\"> due " + d.toLocaleString() +" </span><button class=\"btn btn-sm btn-outline-danger done\" type=\"button\">Done</button>"
+        new_task.innerHTML += description + "<span class=\"due\"> due " + d.toLocaleString() 
     }
     else {
-        new_task.innerHTML += description + "<button class=\"btn btn-sm btn-outline-danger done\" type=\"button\">Done</button>"
+        new_task.innerHTML += description
     }
+    const done_btn = document.createElement("button")
+    done_btn.setAttribute("class", "btn btn-sm btn-outline-danger done")
+    done_btn.setAttribute("type", "button")
+    done_btn.textContent = "Done"
+    done_btn.addEventListener('click', e => {
+        // Whe the link is clicked, call preventDefault,
+        // effectively disabling the link.
+        e.path[1].remove()
+    })
+    new_task.appendChild(done_btn)
     task_list.appendChild(new_task)
 }
 
